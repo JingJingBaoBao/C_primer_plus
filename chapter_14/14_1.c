9c7d0b5fe240a9029e1900fc286577a3ccac04da
Sundaxian <m17824832870@163.com>
2022-08-02 00:15:43 +0800

    learning struct

---
+++ chapter_14/14_1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+char *s_gets(char *st, int n);
+#define MAXTITL 41
+#define MAXAUTL 31
+
+struct book {
+    char title[MAXTITL];
+    char author[MAXAUTL];
+    float value;
+};
+
+int main(void)
+{
+    struct book library;
+    puts("Enter the book title");
+    s_gets(library.title, MAXTITL);
+    puts("Enter the author");
+    s_gets(library.author, MAXAUTL);
+    puts("Now enter the value");
+    scanf("%f", &library.value);
+    printf("%s by %s : $%.2f\n", library.title, library.author, library.value);
+    return 0;
+}
+
+char *s_gets(char * st, int n)
+{
+    char * ret_val;
+    char * find;
+    ret_val = fgets(st, n, stdin);
+    if (ret_val) {
+        find = strchr(st, '\n');
+        if (find) {
+            *find = '\0';
+        }
+        else {
+            while (getchar() != '\n') {
+                continue;
+            }
+        }
+    }
+    return ret_val;
+}
---
+++ chapter_14/14_2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+char * s_gets(char * st, int n);
+#define MAXTITL 40
+#define MAXAUTL 40
+#define MAXBKS 100
+
+struct book {
+    char title[MAXTITL];
+    char author[MAXAUTL];
+    float value;
+};
+
+int main(void)
+{
+    struct book library[MAXBKS];
+    int count = 0;
+    int index;
+    printf("Please enter the book title\n");
+    printf("Please [enter] at the start of a line to stop\n");
+    while (count < MAXBKS && s_gets(library[count].title, MAXTITL) != NULL
+            && library[count].title[0] != '\0') {
+                printf("Now enter the author.\n");
+                s_gets(library[count].author, MAXAUTL);
+                puts("Now enter the value");
+                scanf("%f", &library[count++].value);
+                while (getchar() != '\n') {
+                    continue;
+                }
+                if (count < MAXBKS) {
+                    puts("Enter the next title");
+                }
+            }
+    if (count > 0) {
+        puts("here is the list of your books\n");
+        for (index = 0; index < count; index++) {
+            printf("%s by %s : $%.2f\n", library[index].title,
+            library[index].author, library[index].value);
+        }
+    }
+    else {
+        puts("No books? Too bad.");
+    }
+
+    return 0;
+}
+
+char * s_gets(char * st, int n)
+{
+    char * ret_val;
+    char * find;
+    ret_val = fgets(st, n, stdin);
+    if (ret_val) {
+        find = strchr(st, '\n');
+        if (find) {
+            *find = '\0';
+        }
+        else {
+            while (getchar() != '\n') {
+                continue;
+            }
+        }
+    }
+    return ret_val;
+}
